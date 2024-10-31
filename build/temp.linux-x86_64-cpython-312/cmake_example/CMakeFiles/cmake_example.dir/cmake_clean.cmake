fb3d7cd13c3fa85b7822102254b9fa4ece851187
Yuan SUN <sunyuan860510@gmail.com>
2024-10-31 22:23:52 +0100

    test commit

---
+++ build/temp.linux-x86_64-cpython-312/cmake_example/CMakeFiles/cmake_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/ysun/pybind11_example/build/lib.linux-x86_64-cpython-312/cmake_example.cpython-312-x86_64-linux-gnu.so"
+  "/home/ysun/pybind11_example/build/lib.linux-x86_64-cpython-312/cmake_example.pdb"
+  "CMakeFiles/cmake_example.dir/src/main.cpp.o"
+  "CMakeFiles/cmake_example.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
